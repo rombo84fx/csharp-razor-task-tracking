f220be8224e3f056b03c2bb80fd4ca15a8abd59f
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2020-06-14 23:35:24 -0500

    Creating the Layout and Navigation

---
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tasker.Pages
+{
+    public class CreateTask : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Tasker/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tasker.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
